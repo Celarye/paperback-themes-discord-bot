a6550102a7602324938dca8c41951bd557430072
Celarye <edward.smet@gmail.com>
2022-12-24 17:40:06 +0100

    Updated the pbt command reply.

--- commands/pbt.js
+++ commands/pbt.js
@@ -1,10 +1,18 @@
+const wait = require('node:timers/promises').setTimeout;
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('pbt')
-		.setDescription('Replies with the Paperback Themes embed message.'),
+		.setDescription('Replies with the main Paperback Themes embed message.'),
 	async execute(interaction) {
-		await interaction.reply('Test!');
+		await interaction.reply({ content:'Hello World!', ephemeral: true });
+		await wait(4000);
+		await interaction.followUp({ content: 'Test!', ephemeral: true });
+		const message = await interaction.fetchReply();
+		console.log(message);
+		await wait(4000);
+		await interaction.deleteReply();
+		console.log('Finished the pbt command reply.');
 	},
 };
