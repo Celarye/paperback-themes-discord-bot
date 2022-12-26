8475f693d8cdbded9b3c7c96389506f2cb1a3e81
Celarye <edward.smet@gmail.com>
2022-12-26 22:39:13 +0100

    Made the ping command ephemeral.

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with the ping of the bot.'),
 	async execute(interaction) {
-		const sent = await interaction.reply({ content: `The current websocket latency is ${Math.round(interaction.client.ws.ping)}ms.`, fetchReply: true });
-		await interaction.followUp(`The roundtrip latency is ${sent.createdTimestamp - interaction.createdTimestamp}ms.`);
+		const sent = await interaction.reply({ content: `The current websocket latency is ${Math.round(interaction.client.ws.ping)}ms.`, fetchReply: true, ephemeral: true });
+		await interaction.followUp({ content: `The roundtrip latency is ${sent.createdTimestamp - interaction.createdTimestamp}ms.`, ephemeral: true });
 	},
 };
