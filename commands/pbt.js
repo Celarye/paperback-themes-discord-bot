48df3e7151479c0acce791ef2ed7139e23efbfa4
Eduard Smet <96624179+Celarye@users.noreply.github.com>
2023-01-16 23:52:14 +0100

    A small fix.

--- commands/pbt.js
+++ commands/pbt.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setName('pbt')
 		.setDescription('Replies with the main Paperback Themes embed message.'),
 	async execute(interaction) {
-		await interaction.reply({ content:'Hello World!', ephemeral: true });
+		await interaction.reply({ content: 'Hello World!', ephemeral: true });
 		await wait(4000);
 		await interaction.followUp({ content: 'Test!', ephemeral: true });
 		const message = await interaction.fetchReply();
