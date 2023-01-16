a19b00ec4a52a56601c7e7ce3b848838a1d60136
Eduard Smet <96624179+Celarye@users.noreply.github.com>
2023-01-16 23:51:26 +0100

    Renamed the command file and made some changes which don't work as intended yet.

---
+++ commands/latency.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('latency')
+		.setDescription('Replies with the latency of the bot.')
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('all')
+				.setDescription('Replies with both the latencies of the bot.'))
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('websocket')
+				.setDescription('Replies with the websocket latency of the bot.'))
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('roundtrip')
+				.setDescription('Replies with the roundtrip latency of the bot.')),
+	async execute(interaction) {
+		if (interaction._subcommand == 'websocket') {
+			await interaction.reply({ content: `The current websocket latency of the bot is ${Math.round(interaction.client.ws.ping)}ms.`, ephemeral: true });
+		}
+		else if (interaction._subcommand == 'roundtrip') {
+			const sent = await interaction.reply({ content: 'Checking the roundtrip latency of the bot!', fetchReply: true });
+			await interaction.followUp({ content: `The roundtrip latency of the bot is ${sent.createdTimestamp - interaction.createdTimestamp}ms.`, ephemeral: true });
+		}
+		else {
+			const sent = await interaction.reply({ content: `The current websocket latency of the bot is ${Math.round(interaction.client.ws.ping)}ms.`, fetchReply: true, ephemeral: true });
+			await interaction.followUp({ content: `The roundtrip latency of the bot is ${sent.createdTimestamp - interaction.createdTimestamp}ms.`, ephemeral: true });
+		}
+	},
+};
