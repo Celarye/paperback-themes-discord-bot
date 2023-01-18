89f3d1c4571e90026d2b8bc4fb9c9d9dfc349b56
Eduard Smet <96624179+Celarye@users.noreply.github.com>
2023-01-18 17:53:36 +0100

    Fixed the issues causing it to not work as intended.

--- commands/latency.js
+++ commands/latency.js
@@ -17,11 +17,11 @@ module.exports = {
 				.setName('roundtrip')
 				.setDescription('Replies with the roundtrip latency of the bot.')),
 	async execute(interaction) {
-		if (interaction._subcommand == 'websocket') {
+		if (interaction.options._subcommand == 'websocket') {
 			await interaction.reply({ content: `The current websocket latency of the bot is ${Math.round(interaction.client.ws.ping)}ms.`, ephemeral: true });
 		}
-		else if (interaction._subcommand == 'roundtrip') {
-			const sent = await interaction.reply({ content: 'Checking the roundtrip latency of the bot!', fetchReply: true });
+		else if (interaction.options._subcommand == 'roundtrip') {
+			const sent = await interaction.reply({ content: 'Checking the roundtrip latency of the bot!', fetchReply: true, ephemeral: true });
 			await interaction.followUp({ content: `The roundtrip latency of the bot is ${sent.createdTimestamp - interaction.createdTimestamp}ms.`, ephemeral: true });
 		}
 		else {
