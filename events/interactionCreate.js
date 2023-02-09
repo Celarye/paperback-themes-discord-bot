e98499138bc4be315075d4ab040906839076c187
Eduard Smet <96624179+Celarye@users.noreply.github.com>
2023-02-09 22:27:17 +0100

    Created the interactionCreate event.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,15 +3,16 @@ const { Events } = require('discord.js');
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
+		if (interaction.isStringSelectMenu()) {
+			console.log(interaction);
+			return;
+		}
 		if (!interaction.isChatInputCommand()) return;
-
 		const command = interaction.client.commands.get(interaction.commandName);
-
 		if (!command) {
 			console.error(`No command matching ${interaction.commandName} was found.`);
 			return;
 		}
-
 		try {
 			await command.execute(interaction);
 		}
